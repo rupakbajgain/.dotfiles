13e2e405db6ce4b91297d8f2e4302c36aea22d1b
Rupak Bajgain <bajgainrupakb@gmail.com>
2024-11-25 07:50:06 +0545

    Providing server on won

--- system/web-setup.nix
+++ system/web-setup.nix
@@ -1,9 +1,50 @@
 {lib, config, pkgs, ...}:
+
+let
+  freshrss_package = pkgs.freshrss;
+  freshrss_pool = "freshrss";
+  freshrss_url_path = "rss";
+in
 {
+
+#set my own virtualHost
+#copy from nixpkgs and merge
+      services.nginx = {
+        enable = true;
+        virtualHosts.localhost = {
+          # php files handling
+          # this regex is mandatory because of the API
+          locations."~ ^.+?\.php(/.*)?$"={
+            root = "${freshrss_package}/p";
+            extraConfig = ''
+              fastcgi_pass unix:${config.services.phpfpm.pools.${freshrss_pool}.socket};
+              fastcgi_split_path_info ^(.+\.php)(/.*)$;
+              # By default, the variable PATH_INFO is not set under PHP-FPM
+              # But FreshRSS API greader.php need it. If you have a “Bad Request” error, double check this var!
+              # NOTE: the separate $path_info variable is required. For more details, see:
+              # https://trac.nginx.org/nginx/ticket/321
+              set $path_info $fastcgi_path_info;
+              fastcgi_param PATH_INFO $path_info;
+              include ${pkgs.nginx}/conf/fastcgi_params;
+              include ${pkgs.nginx}/conf/fastcgi.conf;
+            '';
+          };
+
+          locations."/" = {
+            root = "${freshrss_package}/p";
+            tryFiles = "$uri $uri/ index.php";
+            index = "index.php index.html index.htm";
+          };
+        };
+      };
+
+#add services
     services.freshrss = {
       enable = true;
-      baseUrl = "http://localhost";
+      baseUrl = "http://localhost";#change this to tell the base url is new
       passwordFile = pkgs.writeText "password" "secret";
+      virtualHost = null;#dont setup, use previous
+      pool = freshrss_pool;
       dataDir = "/srv/freshrss";
       extensions = [ pkgs.freshrss-extensions.youtube ] ++ [
         (pkgs.freshrss-extensions.buildFreshRssExtension rec {
@@ -25,7 +66,14 @@
         })
       ];
     };
-    #services.rss-bridge = {
-    #    enable = true;
-    #};
+    /*
+    services.rss-bridge = {
+        enable = true;
+        config={
+          system.enabled_bridges = [ "*" ];
+          FileCache = {
+            enable_purge = true;
+          };
+        };
+    };*/
 }
